2caa20bfe471fc5d0c2412bf9475650b4cb84dfc
Beccatoni <rbccmtni1@gmail.com>
2025-03-31 17:46:52 +0200

    create fields for the profile

---
+++ Profile.cs
@@ -0,0 +1,11 @@
+namespace oopiInCsharp;
+
+public class Profile
+{
+   private string name;
+   private int age;
+   private string city;
+   private string country;
+   private string pronouns;
+   private string[] hobbies;
+}
