d5d0bf6badf66d1c3743f5ab48efd65c9f718a0c
Beccatoni <rbccmtni1@gmail.com>
2025-03-31 18:04:11 +0200

    add the profile class

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ using oopiInCsharp;
 
 Console.WriteLine("Hello, World!");
 // Forest f = new Forest {name="Amazon", biome = "Rain forest", trees = 342, age = 200};
-Forest f = new Forest("Amazon");
+var f = new Forest("Amazon");
 Console.WriteLine(f.trees);
 f.Grow();
 Console.WriteLine(f.trees);
 
-Star sun = new Star("Sun", "G-type");
+var sun = new Star("Sun", "G-type");
 sun.Shine();
 sun.GrowOlder();
 sun.Shine();
 sun.Supernova();
 
-Star unknownStar = new Star("Mystery Star");
+var unknownStar = new Star("Mystery Star");
 unknownStar.Shine();
 unknownStar.GrowOlder();
 
@@ -25,3 +25,7 @@ Console.WriteLine(Forest.ForestsCreated);
 Forest f1 = new Forest("Gishwati");
 Forest f2 = new Forest("Nyungwe", "Rain forest");
 Console.WriteLine(Forest.ForestsCreated);
+
+Profile p1 = new Profile("Sam Drakkila", 30, "New york", "USA", "he/him");
+Console.WriteLine(p1.ViewProfile());
+p1.SetHobbies(new string[]{"listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel","reading advice columns"});
