1b3eabc959aa9680ed8ae8914f5868e5e80404e5
Beccatoni <rbccmtni1@gmail.com>
2025-03-31 17:49:07 +0200

    add a constructor for the class

--- Profile.cs
+++ Profile.cs
@@ -8,4 +8,15 @@ public class Profile
    private string country;
    private string pronouns;
    private string[] hobbies;
+
+   public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+   {
+      this.name = name;
+      this.age = age;
+      this.city = city;
+      this.country = country;
+      this.pronouns = pronouns;
+      this.hobbies = new string[] { };
+   }
+   
 }
